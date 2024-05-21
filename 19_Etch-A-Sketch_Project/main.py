6a15d3a5eea9e7766d51188d4240a1c844a54227
Your Name <you@example.com>
2024-05-21 12:31:42 +0530

    Completed Day 19!

--- 19_Etch-A-Sketch_Project/main.py
+++ 19_Etch-A-Sketch_Project/main.py
@@ -1,47 +1,48 @@
 from turtle import Turtle, Screen
-
-sam = Turtle()
-screen = Screen()
-
-def move_forward():
-    sam.forward(50)
-    
-def move_backward():
-    sam.backward(50)
-    
-# def move_counter_clockwise():
-#     sam.circle(20, extent = 180)
-    
-# def move_clockwise():
-#     sam.circle(-20, extent = 180)
-
-def dial_turn_left():
-    """Change the heading to left of the current heading. Move forward 10 steps"""
-    sam.heading()
-    sam.setheading(sam.heading() + 10)
-    
-def dial_turn_right():
-    """Change the heading to left of the current heading. Move forward 10 steps"""
-    sam.setheading(sam.heading() - 10)
-
-
-def clear():
-    sam.clear()
-    sam.penup()
-    sam.home()
-    sam.pendown()
-    """Goes back to the center"""
-    
-""" 
+from random import randint
 
 """
-    
-screen.listen()
-screen.onkey(key="w", fun=move_forward)
-screen.onkey(key="s", fun=move_backward)
-screen.onkey(key="a", fun=dial_turn_left)
-screen.onkey(key="d", fun=dial_turn_right)
-screen.onkey(key="c", fun=clear)
+TODO:
+* Create a text input using screen.textinput
+* Get all 6 turtles in a starting position
+* Once in the starting position, if the user has made a bet, then start the race
+* End the race, once the first turtle has finished
+* Figure out which turtle finished  the race by checking the color of the first turtle using .pencolor()
+* Declare the winner!
+"""
 
+is_race = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "green", "blue", "orange", "yellow", "purple"]
+turtle_list = []
+
+y=-100
+for i in colors:
+    exec(f'turtle_{i} = Turtle(shape="turtle")')
+    exec(f'turtle_{i}.color(i)')
+    exec(f'turtle_{i}.penup()')
+    x = -230
+    exec(f'turtle_{i}.goto(x, y)')
+    y += 40
+    exec(f'turtle_list.append(turtle_{i})')
+    
+
+if user_bet:
+    is_race = True
+    
+while is_race:
+    for turtle in turtle_list:
+        distance = randint(0,10)
+        turtle.forward(distance)
+        if turtle.xcor() > 230:
+            is_race = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+                        
 
 screen.exitonclick()
