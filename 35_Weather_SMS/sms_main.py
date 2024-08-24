351222b117cd7d32a143a44d5f1992412371d6e1
Your Name <you@example.com>
2024-08-24 15:30:54 +0530

    Completed Day 35

--- 35_Weather_SMS/sms_main.py
+++ 35_Weather_SMS/sms_main.py
@@ -4,12 +4,19 @@ import vonage
 
 URL_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 
+# weather_params = {
+#     "lat" : 12.957,
+#     "lon" : 77.401,
+#     "appid" : "ce5d45b3516066fb8fbb8a11b95e04ee",
+#     "cnt" : 4,
+# }       
+
 weather_params = {
-    "lat" : 12.957,
-    "lon" : 77.401,
+    "lat" : 21.59,
+    "lon" : 74.56,
     "appid" : "ce5d45b3516066fb8fbb8a11b95e04ee",
     "cnt" : 4,
-}                
+}               
 
 heather_number = 917619177011
 sharada_number = 917619177022
@@ -25,21 +32,17 @@ for period in weather_data["list"]:
     if int(weather_id) < 700:
         to_bring_umbrella = True
 if to_bring_umbrella:
+    print("Don't forget to bring an umbrella!")
     client = vonage.Client(key="1572d60c", secret="pSsgJ2ocbiIBAYiI")
     sms = vonage.Sms(client)
     responseData = sms.send_message(
     {
         "from": "Heather's Weather Updates",
         "to": f"{heather_number}",
-        "text": "Don't forget to bring an umbrella! ",
-    }
-)
-   
+        "text": "Looks like it might rain today. Don't forget to bring an umbrella! ",
+    })
 
-
-
-
-if responseData["messages"][0]["status"] == "0":
-    print("Message sent successfully.")
-else:
-    print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
+    if responseData["messages"][0]["status"] == "0":
+        print("Message sent successfully.")
+    else:
+        print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
--- 35_Weather_SMS/whatsapp_main.py
+++ 35_Weather_SMS/whatsapp_main.py
@@ -1,18 +1,23 @@
 import requests
-import pprint
-import vonage
+
 
 URL_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 
+## Home Lat/Long
+# weather_params = {
+#     "lat" : 12.957,
+#     "lon" : 77.401,
+#     "appid" : "ce5d45b3516066fb8fbb8a11b95e04ee",
+#     "cnt" : 4,
+# }    
+
+## Fake Lat/Long
 weather_params = {
-    "lat" : 12.957,
-    "lon" : 77.401,
+    "lat" : 21.59,
+    "lon" : 74.56,
     "appid" : "ce5d45b3516066fb8fbb8a11b95e04ee",
     "cnt" : 4,
-}                
-
-heather_number = 917619177011
-sharada_number = 917619177022
+}                   
 
 response = requests.get(URL_ENDPOINT, params = weather_params)
 response.raise_for_status()
@@ -25,21 +30,28 @@ for period in weather_data["list"]:
     if int(weather_id) < 700:
         to_bring_umbrella = True
 if to_bring_umbrella:
-    client = vonage.Client(key="1572d60c", secret="pSsgJ2ocbiIBAYiI")
-    sms = vonage.Sms(client)
-    responseData = sms.send_message(
-    {
-        "from": "Heather's Weather Updates",
-        "to": f"{heather_number}",
-        "text": "Don't forget to bring an umbrella! ",
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
     }
-)
+
+    json_data = {
+        'from': '14157386102',
+        'to': '917619177011',
+        'message_type': 'text',
+        'text': "Looks like it might rain today. Don't forget to bring an umbrella! ",
+        'channel': 'whatsapp',
+    }
+
+    response = requests.post(
+        'https://messages-sandbox.nexmo.com/v1/messages',
+        headers=headers,
+        json=json_data,
+        auth=('1572d60c', 'pSsgJ2ocbiIBAYiI'),
+    )
+    print("Bring an umbrella!")
+    response.raise_for_status()
+
    
 
 
-
-
-if responseData["messages"][0]["status"] == "0":
-    print("Message sent successfully.")
-else:
-    print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
