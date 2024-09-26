ed2723ca0a9740e15bb10a8fc7eb37d7b6beb30f
19hhowton <19hhowton@gmail.com>
2024-09-26 15:25:00 +0200

    Completed

--- 3_Web_Development_Projects/57_Jinja/blog_project/main.py
+++ 3_Web_Development_Projects/57_Jinja/blog_project/main.py
@@ -1,22 +1,28 @@
 from flask import Flask, render_template
+from post import Post
 import requests
 
+posts = requests.get("https://api.npoint.io/22980569028cbfc903db").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
+print(post_objects)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    blog_url = "https://api.npoint.io/22980569028cbfc903db"
-    response = requests.get(url=blog_url)
-    all_posts = response.json()
-    return render_template("index.html", posts=all_posts)
+    return render_template("index.html", posts=post_objects)
 
-@app.route("/post/<num>") #0, 1, 2 @input 1, but it's 0 
+@app.route("/post/<int:num>") 
 def get_article(num):
-    blog_url = "https://api.npoint.io/22980569028cbfc903db"
-    response = requests.get(url=blog_url)
-    all_posts = response.json()
-    return render_template("post.html", post=all_posts[int(num) - 1])
+    requested_post = None
+    for post in post_objects:
+        if post.id == num:
+            requested_post = post
+    print(requested_post)
+    return render_template("post.html", post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- 3_Web_Development_Projects/57_Jinja/blog_project/post.py
+++ 3_Web_Development_Projects/57_Jinja/blog_project/post.py
@@ -1,2 +1,9 @@
+import requests
+
 class Post:
-    pass
+    """Create Post object for each post."""
+    def __init__(self, post_id, title, subtitle, body):
+        self.id = post_id
+        self.title = title
+        self.subtitle = subtitle
+        self.body = body
