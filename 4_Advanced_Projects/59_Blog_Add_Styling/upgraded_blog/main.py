37e5046ec3937a1086818d433b3b157a544dc0be
19hhowton <19hhowton@gmail.com>
2024-10-07 15:53:26 +0200

    Day 59 Complete

--- 4_Advanced_Projects/59_Blog_Add_Styling/upgraded_blog/main.py
+++ 4_Advanced_Projects/59_Blog_Add_Styling/upgraded_blog/main.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import requests
 from post import Post
-from sendemail import send_email
 
 #https://www.npoint.io/docs/607b901b39eb60f3737f
 
@@ -22,21 +21,9 @@ def home():
 def about_page():
     return render_template("about.html")
 
-@app.route("/contact", methods=['GET','POST'])
-def contact():
-    if request.method == 'GET':
-        return render_template("contact.html", submit=False)
-    elif request.method == 'POST':
-        data = request.form
-
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])     
-        
-        send_email(data["name"], data["email"], data["phone"], data["message"])
-
-        return render_template("contact.html", submit=True)
+@app.route('/contact')
+def contact_page():
+    return render_template("contact.html")
 
 @app.route('/<num>')
 def article_page(num):
