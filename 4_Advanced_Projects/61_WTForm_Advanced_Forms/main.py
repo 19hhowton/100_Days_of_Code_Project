456a19b719e4989f5c9d822a880d4290ea4741d8
19hhowton <19hhowton@gmail.com>
2024-10-08 14:18:01 +0200

    In Progress 61

--- 4_Advanced_Projects/61_WTForm_Advanced_Forms/main.py
+++ 4_Advanced_Projects/61_WTForm_Advanced_Forms/main.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "this is my secret"
 
-@app.route("/")
-def home():
-    return render_template('login.html')
+
+class LoginForm(FlaskForm):
+    email = StringField("Email: ", validators=[InputRequired(), Email()])
+    password = PasswordField("Password: ", validators=[InputRequired(), Length(min=8)])
+    submit = SubmitField("Submit")
+
+@app.route("/", methods=['GET', 'POST'])
+def login():
+    email = None
+    password = None
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return redirect('/success')
+        else:
+            return redirect('/denied')
+    return render_template("login.html", login_form = login_form)
+
+@app.route("/success")
+def success():
+    return render_template('success.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
